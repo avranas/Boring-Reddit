33b1520877c6ec09f3de036bb739e25f7d600dfe
Alex Vranas <avranas42@gmail.com>
2024-01-13 18:58:02 -0800

    Update file serve path

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || '3000';
 
 console.log(path.join(__dirname, "../dist"))
 console.log(path.join(__dirname, "../dist/index.html"))
-app.use(express.static(path.join(__dirname, "../../dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../../dist/index.html"));
